3c2fc63b9ea43ad73163786bae557aecd841f78f
Hiren Joshi <hirenj@gmail.com>
2015-10-27 21:44:42 +0100

    Start speccing out by tests

---
+++ test/sequences/test-iupac.js
@@ -0,0 +1,7 @@
+QUnit.module("Test that we can read in sequences", {
+});
+
+QUnit.test( "Reading in a simple monosaccharide" , function( assert ) {
+	var sequence = 'GlcNAc';
+	assert.ok(false,"Can't read in sequence");
+});
