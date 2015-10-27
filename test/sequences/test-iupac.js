34b69d4346528b5324d49b6050f07aeebcbc399f
Hiren Joshi <hirenj@gmail.com>
2015-10-27 22:07:09 +0100

    Filling out the tests for sequences

--- test/sequences/test-iupac.js
+++ test/sequences/test-iupac.js
@@ -7,3 +7,13 @@ QUnit.test( "Reading in a simple monosaccharide" , function( assert ) {
   var sequence = "GlcNAc";
   assert.ok(false,"Can't read in sequence "+sequence);
 });
+
+QUnit.test( "Reading in a disaccharide", function( assert ) {
+  var sequence = "GlcNAc(b1-4)GlcNAc";
+  assert.ok(false,"Can't read in sequence "+sequence);
+});
+
+QUnit.test( "Reading in an incorrect sequence" , function( assert ) {
+  var sequence = "Shazbut";
+  assert.ok(false,"Can't read in sequence "+sequence);
+});
