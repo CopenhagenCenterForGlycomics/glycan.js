cb4296d3c25f88eb080ace70d75ba8a9db474f5e
Hiren Joshi <hirenj@gmail.com>
2016-04-21 11:33:42 +0200

    List of methods needed to be implemented

---
+++ js/Sugar.js
@@ -0,0 +1,29 @@
+export default class Sugar {
+
+	// FIXME - Matching residues (give a prototype, which residues match)
+
+	// Positional description
+	// FIXME - linkage + residue path from given residue to root
+	// FIXME - linkages from a residue to the root
+	// FIXME - get path to root
+	// FIXME - leaves
+	// FIXME - paths (all paths from leaves to root)
+
+	// Math functions
+	// FIXME - Compare by block - run the closure across the sugar,
+	//			given a traversal algorithm
+	// FIXME - Union - Create a union sugar from two sugars
+	// FIXME - Subtract - Get residues that aren't common
+	// FIXME - Intersect - Find the residues that match with given sugar
+
+	// Search methods
+	// FIXME - Find residue by linkage path
+	// FIXME - Find residue by linkage and residue identifier pair path
+	//			Specify the start residues to start looking from.
+	//			Allow wildcards for the residue identifier
+
+	// Traversal methods
+	// FIXME - Depth first traversal
+	// FIXME - Breadth first traversal
+	// FIXME - Node to root traversal
+}
