e3abac4f4fd0c5be60c92f62a6f080309035072a
Hiren Joshi <hirenj@gmail.com>
2016-05-03 12:11:24 +0200

    Add in reading and writing support for ambiguous sequences

--- js/CondensedIupac.js
+++ js/CondensedIupac.js
@@ -18,21 +18,29 @@ create_bold_branch = (units) => {
 		throw new Error("Empty branch");
 	}
 	let [child_root, linkage] = unit.split("(");
-	create_bold_tree(child_root,units);
-	return [ child_root, linkage ];
+	let child = get_monosaccharide(child_root);
+	create_bold_tree(child,units);
+	return [ child, linkage ];
 };
 
 create_bold_tree = ( root, units ) => {
 	while (units.length > 0) {
 		let unit = units.shift();
 		if ( unit == "]" ) {
-			let [branch_root, linkage] = create_bold_branch(units);
-			root.addChild(branch_root,linkage);
+			let [child, linkage] = create_bold_branch(units);
+			let [anomer,parent_link,,child_link] = (linkage || "").split("");
+			child.anomer = anomer;
+			child.parent_linkage = parseInt(parent_link);
+			root.addChild(parseInt(child_link),child);
 		} else if ( unit == "[" ) {
 			return;
 		} else {
 			let [child_root, linkage] = unit.split("(");
-			root.addChild(parseInt(linkage.split(/-/)[1]),get_monosaccharide(child_root));
+			let [anomer,parent_link,,child_link] = (linkage || "").split("");
+			let child = get_monosaccharide(child_root);
+			child.anomer = anomer;
+			child.parent_linkage = parseInt(parent_link);
+			root.addChild(parseInt(child_link),child);
 		}
 	}
 };
@@ -42,26 +50,84 @@ let write_monosaccharide = (mono) => {
 	return mono.toString();
 };
 
+let write_linkage = (mono) => {
+	if (! mono.parent ) {
+		return "";
+	}
+	return "("+ mono.anomer + mono.parent_linkage + "-";
+};
+
 let reverse = function(string) {
 	return string.split("").reverse().join("");
 };
 
-let Builder = (superclass) => class extends superclass {
-	parseSequence(sequence) {
-		let units = sequence.split(/[\[\]]/);
-		units = [].concat.apply([],units.map(unit => reverse(unit).split(")").map(reverse)));
-		let root = get_monosaccharide( units.shift() );
-		create_bold_tree(root,units);
-		return root;
+let getPropertyDescriptor = function(object,descriptor) {
+	let retval = null;
+	while (! (retval = Object.getOwnPropertyDescriptor(object,descriptor)) && Object.getPrototypeOf(object) ) {
+		object = Object.getPrototypeOf(object);
 	}
+	return retval;
 };
 
-let Writer = (superclass) => class extends superclass {
-	writeSequence(start=this.root) {
-		let self = this;
-		let child_sequence = ""+start.children.map(kid => self.writeSequence(kid)).reduce( (curr,next) => curr ? "["+next+"]"+curr : next );
-		return child_sequence+write_monosaccharide(start);
+let Builder = function(superclass) {
+	let getter = (getPropertyDescriptor(superclass.prototype, "sequence") || { "get" : null }).get;
+	let setter = function(sequence) {
+		this.root = this.parseSequence(sequence);
+	};
+	let methods = {};
+	if (getter) {
+		methods.get = getter;
 	}
+	if (setter) {
+		methods.set = setter;
+	}
+	Object.defineProperty(superclass.prototype, "sequence", methods);
+
+	return class extends superclass {
+		parseSequence(sequence) {
+			let units = sequence.split(/([\[\]])/);
+			let branch_count = units.length;
+			units = [].concat.apply([],units.map(unit => reverse(unit).split(")").filter( (unit) => unit.length ).map(reverse))).reverse();
+			if (branch_count == 1) {
+				units = units.reverse();
+			}
+			let root = get_monosaccharide( units.shift() );
+			create_bold_tree(root,units);
+			return root;
+		}
+	};
 };
+
+let link_expander = function(links) {
+	let position = links[0];
+	return links[1].map( (mono) => [ position , mono ]);
+};
+
+let Writer = function(superclass) {
+	let setter = (getPropertyDescriptor(superclass.prototype, "sequence") || { "set" : null }).set;
+	let getter = function() {
+		return this.writeSequence(this.root);
+	};
+	let methods = {};
+
+	if (getter) {
+		methods.get = getter;
+	}
+	if (setter) {
+		methods.set = setter;
+	}
+
+	Object.defineProperty(superclass.prototype, "sequence", methods);
+
+	return class extends superclass {
+		writeSequence(start=this.root) {
+			let self = this;
+			let child_sequence = ""+[].concat.apply([],[...start.child_linkages].map(link_expander)).map( kid => self.writeSequence(kid[1])+kid[0]+")" ).reduce( (curr,next) => curr ? curr+"["+next+"]" : next , "" );
+			return child_sequence+write_monosaccharide(start)+write_linkage(start);
+		}
+	};
+};
+
+
 
 export {Builder,Writer};
--- test/sequences/test-iupac.js
+++ test/sequences/test-iupac.js
@@ -2,10 +2,10 @@
 
 import Monosaccharide from "../../js/Monosaccharide";
 import Sugar from "../../js/Sugar";
-import {Builder} from "../../js/CondensedIupac";
+import {Builder,Writer} from "../../js/CondensedIupac";
 
 
-class IupacSugar extends Builder(Sugar) {}
+class IupacSugar extends Builder(Writer(Sugar)) {}
 
 QUnit.module("Test that we can read in sequences", {
 });
@@ -19,11 +19,34 @@ QUnit.test( "Reading in a simple monosaccharide" , function( assert ) {
 QUnit.test( "Reading in a disaccharide", function( assert ) {
   var sequence = "GlcNAc(b1-4)GlcNAc";
   let sugar = new IupacSugar();
-  let root = sugar.parseSequence(sequence);
-  assert.ok(root.identifier == "GlcNAc","Root is set correctly");
-  assert.ok(root.children.length == 1,"Has the right number of children");
-  assert.ok(root.children[0].identifier == "GlcNAc","Has the right child");  
+  sugar.sequence = sequence;
+  assert.ok(sugar.root.identifier == "GlcNAc","Root is set correctly");
+  assert.ok(sugar.root.children.length == 1,"Has the right number of children");
+  assert.ok(sugar.root.children[0].identifier == "GlcNAc","Has the right child");
 });
+
+QUnit.test( "Reading and writing a disaccharide", function( assert ) {
+  var sequence = "GalNAc(b1-4)GlcNAc";
+  let sugar = new IupacSugar();
+  sugar.sequence = sequence;
+  let regenerated = (sugar.sequence);
+  assert.ok(sugar.root.identifier == "GlcNAc","Root is set correctly");
+  assert.ok(sugar.root.children.length == 1,"Has the right number of children");
+  assert.ok(sugar.root.children[0].identifier == "GalNAc","Has the right child");
+  assert.ok(regenerated == sequence, "Has the same sequence regenerated");
+});
+
+QUnit.test( "Reading and writing a basic mutisugar", function( assert ) {
+  var sequence = "GalNAc(b1-4)[Man(b1-4)]GlcNAc";
+  let sugar = new IupacSugar();
+  sugar.sequence = sequence;
+  let regenerated = (sugar.sequence);
+  assert.ok(sugar.root.identifier == "GlcNAc","Root is set correctly");
+  assert.ok(sugar.root.children.length == 2,"Has the right number of children");
+  assert.ok(sugar.root.children[0].identifier == "Man" ,"Has the right child");
+  assert.ok(regenerated == "Man(b1-4)[GalNAc(b1-4)]GlcNAc", "Has the same sequence regenerated");
+});
+
 
 QUnit.test( "Reading in an incorrect sequence" , function( assert ) {
   var sequence = "Shazbut";
