408cb46eee206f7f4cdad43dca1e97f82675f5b0
Hiren Joshi <hirenj@gmail.com>
2017-09-21 14:47:14 +0200

    Add in build command

---
+++ js/index.js
@@ -0,0 +1,5 @@
+export {default as Sugar} from './Sugar';
+export {default as Monosaccharide} from './Monosaccharide';
+export {default as Reaction} from './Reaction';
+import * as CondensedIupac from './CondensedIupac';
+export { CondensedIupac };
