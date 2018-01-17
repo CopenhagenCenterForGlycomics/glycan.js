ee5cc32bb7ea59a66232c9090aaa383ec72aa337
Hiren Joshi <hirenj@gmail.com>
2018-01-17 22:11:33 +0100

    Fix for simple chains

--- js/SugarAwareLayout.js
+++ js/SugarAwareLayout.js
@@ -19,8 +19,9 @@ class SugarAwareLayout extends CondensedLayout {
       position.dx += (position.dx < 0) ? -0.25 : 0.25;
       return position;
     }
+
     if (res.siblings.filter( sibling => ['Fuc','NeuAc'].indexOf(sibling.identifier) < 0 ).length == 0 &&
-        res.siblings.filter( sibling => ['Fuc','NeuAc'].indexOf(sibling.identifier) >= 0 ).length == 1
+        res.siblings.filter( sibling => ['Fuc','NeuAc'].indexOf(sibling.identifier) >= 0 ).length <= 1
         ) {
       position.dx = 0;
     }
