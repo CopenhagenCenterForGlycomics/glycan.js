473b3d0a61a810174ad7fa207f0840dd4e5f6974
Hiren Joshi <hirenj@gmail.com>
2018-04-23 10:02:38 +0200

    Test sequence support

--- test/reactions/test-testReactionJson.js
+++ test/reactions/test-testReactionJson.js
@@ -12,102 +12,135 @@ class IupacSugar extends Iupac(Sugar) {}
 
 const MGAT5B = 'GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn+\"{GlcNAc(b1-6)}@y5b\"';
 const CHSY1 = ['GalNAc(b1-4)*(u?-?)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GlcA(b1-3)}@y7a\"',
-               'GlcA(b1-3)GalNAc(b1-4)*(u?-?)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GalNAc(b1-4)}@y8a\"',
-               'GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GalNAc(b1-4)}@y5a\"'
+               'GlcA(b1-3)GalNAc(b1-4)*(u?-?)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GalNAc(b1-4)}@y8a\"'
               ];
 
+const EXT1 = [  'GlcNAc(a1-4)*(u?-?)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GlcA(b1-4)}@y7a\"',
+                'GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GlcNAc(a1-4)}@y5a\"',
+                'GlcA(b1-4)GlcNAc(a1-4)*(u?-?)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GlcNAc(a1-4)}@y8a\"'
+             ];
+
+const CSGALNACT1 = ['GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser+\"{GalNAc(b1-4)}@y5a\"'];
+
+const DERMATAN_SEQUENCE = 'GlcNAc(a1-4)GlcA(b1-4)GlcNAc(a1-4)GlcA(b1-4)GlcNAc(a1-4)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser';
+const CHONDROITIN_SEQUENCE = 'GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser';
+
+const NLINKED_CORE = 'GlcNAc(b1-2)Man(a1-3)[GlcNAc(b1-6)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn';
+
+let make_reaction_group = (reaction_seq) => {
+  let reaction = new IupacReaction();
+  reaction.sequence = reaction_seq;
+
+  let reactionset = new ReactionSet();
+
+  reactionset.addReactionRule(reaction);
+
+  let reactiongroup = new ReactionGroup();
+
+  reactiongroup.addReactionSet(reactionset);
+
+  return reactiongroup;
+};
 
 QUnit.module('Test that we can execute ReactionSets', {
 });
 
 QUnit.test( 'MGAT5B works' , function( assert ) {
-  let end_sequence = 'GlcNAc(b1-2)Man(a1-3)[GlcNAc(b1-6)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn';
-  let reaction = new IupacReaction();
-  reaction.sequence = MGAT5B;
+  let end_sequence = NLINKED_CORE;
 
-  let reactionset = new ReactionSet();
-
-  reactionset.addReactionRule(reaction);
-
-  let reactiongroup = new ReactionGroup();
-
-  reactiongroup.addReactionSet(reactionset);
+  let reactiongroup = make_reaction_group(MGAT5B);
 
   let test_sugar = new IupacSugar();
   test_sugar.sequence = end_sequence;
 
-  let supported_tag = reactiongroup.supportLinkages(test_sugar);
-  let supported = test_sugar.composition_for_tag(supported_tag);
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
   assert.ok(supported.length === 1);
-  assert.ok(supported.map( res => res.identifier ).join(',') === 'GlcNAc');
+  assert.deepEqual(supported.map( res => res.identifier ),Array(1).fill('GlcNAc'));
   assert.equal(test_sugar.location_for_monosaccharide(supported[0]), 'y6b' );
 });
 
-QUnit.test( 'CHSY1 works for GlcA' , function( assert ) {
-  let end_sequence = 'GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser';
-  let reaction = new IupacReaction();
-  reaction.sequence = CHSY1[0];
-
-  let reactionset = new ReactionSet();
-
-  reactionset.addReactionRule(reaction);
-
-  let reactiongroup = new ReactionGroup();
-
-  reactiongroup.addReactionSet(reactionset);
+QUnit.test( 'CHSY1 works for GlcA extension' , function( assert ) {
+  let end_sequence = CHONDROITIN_SEQUENCE;
+  let reactiongroup = make_reaction_group(CHSY1[0]);
 
   let test_sugar = new IupacSugar();
   test_sugar.sequence = end_sequence;
 
-  let supported_tag = reactiongroup.supportLinkages(test_sugar);
-  let supported = test_sugar.composition_for_tag(supported_tag);
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
   assert.ok(supported.length === 2);
-  assert.equal(supported.map( res => res.identifier ).join(','),'GlcA,GlcA');
+  assert.deepEqual(supported.map( res => res.identifier ),Array(2).fill('GlcA'));
   assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y7a','y9a']);
 });
 
-QUnit.test( 'CHSY1 works for GalNAc' , function( assert ) {
-  let end_sequence = 'GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser';
-  let reaction = new IupacReaction();
-  reaction.sequence = CHSY1[1];
-
-  let reactionset = new ReactionSet();
-
-  reactionset.addReactionRule(reaction);
-
-  let reactiongroup = new ReactionGroup();
-
-  reactiongroup.addReactionSet(reactionset);
+QUnit.test( 'CHSY1 works for GalNAc extension only' , function( assert ) {
+  let end_sequence = CHONDROITIN_SEQUENCE;
+  let reactiongroup = make_reaction_group(CHSY1[1]);
 
   let test_sugar = new IupacSugar();
   test_sugar.sequence = end_sequence;
 
-  let supported_tag = reactiongroup.supportLinkages(test_sugar);
-  let supported = test_sugar.composition_for_tag(supported_tag);
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
   assert.equal(supported.length,2);
-  assert.equal(supported.map( res => res.identifier ).join(','),'GalNAc,GalNAc');
+  assert.deepEqual(supported.map( res => res.identifier ),Array(2).fill('GalNAc'));
   assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y8a','y10a']);
 });
 
-QUnit.test( 'CHSY1 works for GalNAc' , function( assert ) {
-  let end_sequence = 'GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)GalNAc(b1-4)GlcA(b1-3)Gal(b1-3)Gal(b1-4)Xyl(b1-O)Ser';
-  let reaction = new IupacReaction();
-  reaction.sequence = CHSY1[2];
-
-  let reactionset = new ReactionSet();
-
-  reactionset.addReactionRule(reaction);
-
-  let reactiongroup = new ReactionGroup();
-
-  reactiongroup.addReactionSet(reactionset);
+QUnit.test( 'CSGALNACT1 works for GalNAc init' , function( assert ) {
+  let end_sequence = CHONDROITIN_SEQUENCE;
+  let reactiongroup = make_reaction_group(CSGALNACT1[0]);
 
   let test_sugar = new IupacSugar();
   test_sugar.sequence = end_sequence;
 
-  let supported_tag = reactiongroup.supportLinkages(test_sugar);
-  let supported = test_sugar.composition_for_tag(supported_tag);
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
   assert.equal(supported.length,1);
-  assert.equal(supported.map( res => res.identifier ).join(','),'GalNAc');
+  assert.deepEqual(supported.map( res => res.identifier ),['GalNAc']);
   assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y6a']);
+});
+
+
+QUnit.test( 'EXT1 works on GlcA chain extension' , function( assert ) {
+  let end_sequence = DERMATAN_SEQUENCE;
+  let reactiongroup = make_reaction_group(EXT1[0]);
+
+  let test_sugar = new IupacSugar();
+  test_sugar.sequence = end_sequence;
+
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
+  assert.equal(supported.length,2);
+  assert.deepEqual(supported.map( res => res.identifier ),Array(2).fill('GlcA'));
+  assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y7a','y9a']);
+});
+
+QUnit.test( 'EXT1 works on GlcNAc initiation' , function( assert ) {
+  let end_sequence = DERMATAN_SEQUENCE;
+  let reactiongroup = make_reaction_group(EXT1[1]);
+
+  let test_sugar = new IupacSugar();
+  test_sugar.sequence = end_sequence;
+
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
+  assert.equal(supported.length,1);
+  assert.deepEqual(supported.map( res => res.identifier ),Array(1).fill('GlcNAc'));
+  assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y6a']);
+});
+
+QUnit.test( 'EXT1 works on GlcNAc chain extension' , function( assert ) {
+  let end_sequence = DERMATAN_SEQUENCE;
+  let reactiongroup = make_reaction_group(EXT1[2]);
+
+  let test_sugar = new IupacSugar();
+  test_sugar.sequence = end_sequence;
+
+  let supported = test_sugar.composition_for_tag(reactiongroup.supportLinkages(test_sugar));
+
+  assert.equal(supported.length,2);
+  assert.deepEqual(supported.map( res => res.identifier ),Array(2).fill('GlcNAc'));
+  assert.deepEqual(supported.map( mono => test_sugar.location_for_monosaccharide(mono)), ['y8a','y10a']);
 });
