ce1e5cb2b2d72ccd7d1cde46384f9c7cf1a8d597
Hiren Joshi <hirenj@gmail.com>
2025-07-07 21:18:13 +0200

    Test reduced sugars

--- test/fragment/test-gwb-compatibility.js
+++ test/fragment/test-gwb-compatibility.js
@@ -2,7 +2,7 @@
 
 import Sugar from '../../js/Sugar';
 
-import { Mass, UNDERIVATISED, PERMETHYLATED, REDUCING_END_2AB, NA } from '../../js/Mass';
+import { Mass, UNDERIVATISED, PERMETHYLATED, REDUCING_END_2AB, REDUCING_END_REDUCED, NA } from '../../js/Mass';
 
 import Fragmentor from '../../js/Fragmentor';
 
@@ -115,5 +115,55 @@ QUnit.test( 'GWB_output_2ab 2AB fragments work' , function( assert ) {
   test_fragment(assert,sugar,'z5b',1871.6929);
   test_fragment(assert,sugar,'y5b',1889.7035);
 
+  assert.close(sugar.mass,2051.7563,1e-04,'Full sugar mass is correct');
+
+
+});
+
+
+QUnit.test( 'GWB_output_reduced REDUCED fragments work' , function( assert ) {
+  let sugar = new IupacSugar();
+  sugar.sequence = 'NeuAc(a2-3)Gal(b1-4)GlcNAc(b1-2)Man(a1-3)[Gal(b1-4)GlcNAc(b1-4)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc';
+  sugar.reducing_end = REDUCING_END_REDUCED;
+
+  test_fragment(assert,sugar,'b2b',162.0528);
+  test_fragment(assert,sugar,'c2b',180.0634);
+  test_fragment(assert,sugar,'z1a',205.0950);
+  test_fragment(assert,sugar,'y1a',223.1056);
+  test_fragment(assert,sugar,'b1a',291.0954);
+  test_fragment(assert,sugar,'c1a',309.1060);
+  test_fragment(assert,sugar,'b3b',365.1322);
+  test_fragment(assert,sugar,'c3b',383.1428);
+  test_fragment(assert,sugar,'z2a',408.1744);
+  test_fragment(assert,sugar,'y2a',426.1850);
+  test_fragment(assert,sugar,'b2a',453.1482);
+  test_fragment(assert,sugar,'c2a',471.1588);
+  test_fragment(assert,sugar,'b4b',527.1850);
+  test_fragment(assert,sugar,'c4b',545.1956);
+  test_fragment(assert,sugar,'b3a',656.2276);
+  test_fragment(assert,sugar,'c3a',674.2382);
+  test_fragment(assert,sugar,'b4a',818.2804);
+  test_fragment(assert,sugar,'c4a',836.2910);
+  test_fragment(assert,sugar,'z3a',1097.4122);
+  test_fragment(assert,sugar,'y3a',1115.4228);
+  test_fragment(assert,sugar,'y4a',1277.4756);
+  test_fragment(assert,sugar,'z3b',1388.5076);
+  test_fragment(assert,sugar,'y3b',1406.5182);
+  test_fragment(assert,sugar,'z5a',1462.5444);
+  test_fragment(assert,sugar,'y5a',1480.5550);
+  test_fragment(assert,sugar,'b5a',1507.5182);
+  test_fragment(assert,sugar,'c5a',1525.5288);
+
+  test_fragment(assert,sugar,'z4b',1550.5604);
+  test_fragment(assert,sugar,'y4b',1568.5710);
+  test_fragment(assert,sugar,'z6a',1624.5972);
+  test_fragment(assert,sugar,'y6a',1642.6078);
+
+  test_fragment(assert,sugar,'b6a',1710.5976);
+  test_fragment(assert,sugar,'c6a',1728.6082);
+  test_fragment(assert,sugar,'z5b',1753.6398);
+  test_fragment(assert,sugar,'y5b',1771.6504);
+
+  assert.close(sugar.mass,1933.7032,1e-04,'Full sugar mass is correct');
 
 });
